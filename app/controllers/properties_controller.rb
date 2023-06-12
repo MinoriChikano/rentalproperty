c6a489127153c4a85a4926cfa4ce3b99babdd772
minori chikano <bcloudabc@gmail.com>
2023-06-12 15:40:27 +0900

    showmアクション実装

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,6 +1,6 @@
 class PropertiesController < ApplicationController
   def index
-
+    @properties = Property.all
   end
 
   def new
@@ -8,12 +8,17 @@ class PropertiesController < ApplicationController
   end
 
   def create
-    
+    Property.create(property_params)
+    redirect_to new_property_path
+  end
+
+  def show
+    @property = Property.find(params[:id])
   end
 
   private
 
   def property_params
-    params.require(:property).permit()
+    params.require(:property).permit(:property_name, :rent, :address, :age, :remark)
   end
 end
