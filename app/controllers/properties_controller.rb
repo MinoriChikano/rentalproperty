a7ef1036f7da355844843d282aeb691a1020b295
minori chikano <bcloudabc@gmail.com>
2023-06-13 00:43:04 +0900

    最寄駅情報の表示

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,32 +1,34 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: [:show, :edit, :update, :destroy]
+  before_action :set_nearest_stations, only: [:show, :update, :edit]
+
   def index
     @properties = Property.all
   end
 
   def new
     @property = Property.new
+    2.times { @property.nearest_stations.build }
   end
 
   def create
-    @property = Property.create(property_params)
+    @property = Property.new(property_params)
     if @property.save
       redirect_to properties_path, notice: "物件を登録しました"
     else
       render :new
+      2.times { @property.nearest_stations.build }
     end
   end
 
   def show
-    @property = Property.find(params[:id])
   end
 
   def edit
-    @property = Property.find(params[:id])
+    @property.nearest_stations.build
   end
 
   def update
-    @property = Property.find(params[:id])
     if @property.update(property_params)
       redirect_to properties_path, notice: "編集しました"
     else
@@ -40,12 +42,24 @@ class PropertiesController < ApplicationController
   end
 
   private
-
-  def property_params
-    params.require(:property).permit(:property_name, :rent, :address, :age, :remark)
-  end
-
+  
   def set_property
     @property = Property.find(params[:id])
   end
+
+  def set_nearest_stations
+    @nearest_stations = @property.nearest_stations
+  end
+
+  def property_params
+    params.require(:property).permit(
+      :property_name, 
+      :rent, 
+      :address, 
+      :age, 
+      :remark, 
+      nearest_stations_attributes: [:id, :route_name, :station_name, :on_foot_time]
+    )
+  end
+
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,5 @@
 class Property < ApplicationRecord
    validates :property_name, :rent, :address, :age,  presence: true
    has_many :nearest_stations
+   accepts_nested_attributes_for :nearest_stations
 end
