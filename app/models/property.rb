b7052cf135f24e36f058bd5d9b8634bfbde2786a
minori chikano <bcloudabc@gmail.com>
2023-06-13 02:13:08 +0900

    編集・新規のレイアウト編集

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,5 @@
 class Property < ApplicationRecord
    validates :property_name, :rent, :address, :age,  presence: true
-   has_many :nearest_stations
-   accepts_nested_attributes_for :nearest_stations
+   has_many :nearest_stations, dependent: :destroy 
+   accepts_nested_attributes_for :nearest_stations, reject_if: :all_blank
 end
