86991c95c53ed16d90b714892e49a9b010989a7c
minori chikano <bcloudabc@gmail.com>
2023-06-12 17:36:44 +0900

    物件モデルと最寄り駅モデルをアソシエート

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_12_050749) do
+ActiveRecord::Schema.define(version: 2023_06_12_083436) do
+
+  create_table "nearest_stations", force: :cascade do |t|
+    t.string "route_name"
+    t.string "station_name"
+    t.integer "on_foot_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "property_id", null: false
+    t.index ["property_id"], name: "index_nearest_stations_on_property_id"
+  end
 
   create_table "properties", force: :cascade do |t|
     t.string "property_name"
@@ -22,4 +32,5 @@ ActiveRecord::Schema.define(version: 2023_06_12_050749) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "nearest_stations", "properties"
 end
